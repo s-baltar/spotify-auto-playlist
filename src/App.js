f68b638def345edde33f92baac3832c75d60ae0c
s-baltar <32884123+s-baltar@users.noreply.github.com>
2020-09-06 16:16:56 -0700

    pii-001:Implement login button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import queryString from 'query-string';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+    let parsed = queryString.parse(window.location.search);
+    let accessToken = parsed.access_token;
+    if (!accessToken) 
+      return;
+
+    fetch('https://api.spotify.com/v1/me', 
+          { headers: {'Authorization': 'Bearer ' + accessToken} }).then( 
+            (response) => response.json() ).then(
+              data => this.setState({
+                user: {
+                  name: data.display_name
+                }
+              }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.user ?
+        <div>
+        {this.state.user.name}
+        </div> : <button onClick={() => {
+            window.location = window.location.href.includes("localhost")
+              ? 'http://localhost:8888/login'
+              : 'https://auto-playlist-backend.herokuapp.com/login'
+              }
+            }>Login with Spotify</button>
+        }
+
+      </div>
+    );
+  }
 }
+
 
 export default App;
